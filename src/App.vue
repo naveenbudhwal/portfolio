75fc7be457d6d7a3087be0cbbf6c03936131535a
naveenbudhwal <naveenbudhwal@gmail.com>
2019-08-19 16:42:00 +0530

    Adding nav links

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/skills">Skills</router-link>
+      <router-link to="/gallery">Gallery</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
