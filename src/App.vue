20a06ca6aab590c9e339f5984bd921a6d5adacdd
naveenbudhwal <naveenbudhwal@gmail.com>
2019-08-20 16:24:33 +0530

    Added Navbar and styling

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,70 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/skills">Skills</router-link>
-      <router-link to="/gallery">Gallery</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- <div id="nav">
+      <div class="nav-logo">
+        <router-link to="/">LOGO</router-link>
+      </div>
+      <div class="nav-links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/skills">Skills</router-link>
+        <router-link to="/gallery">Gallery</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>-->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent pt-4">
+      <router-link to="/">LOGO</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse pr-4" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-2">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link to="/skills">Skills</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link to="/gallery">Gallery</router-link>
+          </li>
+          <li class="nav-item mx-2">
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+/* #app {
+  font-family: "Raleway", sans-serif;
+} */
+nav {
+  font-family: "Raleway", sans-serif;
+  font-weight: 600;
+  font-size: 1.2em;
 }
 
-#nav a {
-  font-weight: bold;
+nav a {
   color: #2c3e50;
+  margin: 0 10px;
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
