9916550be71106fbdf5029b2c058183a1a2a1a3a
naveenbudhwal <naveenbudhwal@gmail.com>
2019-08-20 17:05:06 +0530

    Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="root">
+    <div class="intro">
+      Hi
+      <span>,</span> I'm Naveen
+      <span>:)</span>
+    </div>
+    <div class="sub-intro">Web Developer and coder</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
+
+<style scoped>
+.intro {
+  margin-top: 70px;
+  text-align: center;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  font-size: 5rem;
+  color: #434655;
+}
+
+.sub-intro {
+  text-align: center;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+  margin-top: 20px;
+  font-size: 2rem;
+  color: #434655;
+}
+
+span {
+  color: #2b70dc;
+}
+</style>
