39c095a2c3889b95b7798b25ca22e2048663e4dd
naveenbudhwal <naveenbudhwal@gmail.com>
2019-08-23 11:07:00 +0530

    Adding skills page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Skills from './views/Skills.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: Skills
     },
     {
       path: '/about',
---
+++ src/views/Skills.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="heading">Skills</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.heading {
+  font-family: "Montserrat", sans-serif;
+  font-size: 3em;
+  font-weight: 500;
+  color: #434655;
+}
+</style>
