67a336193d07e8082c8892b7ff86fa6da98370cf
naveenbudhwal <naveenbudhwal@gmail.com>
2019-10-06 11:12:37 +0530

    Designing skills page

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="root">
+    <div class="picture">
+      <img :src="skillPicture" alt="Skill Illustration">
+    </div>
     <div class="header">{{skillHeading}}</div>
     <hr>
     <div class="body">
@@ -14,6 +17,7 @@
 export default {
   name: "SkillCard",
   props: {
+    skillPicture: String,
     skillHeading: String,
     skillBody: Array
   }
@@ -22,22 +26,42 @@ export default {
 
 <style scoped>
 .root {
+  width: 50%;
   font-family: "Montserrat", sans-serif;
   border: 1px solid #dadada;
-  border-radius: 20px;
+  border-radius: 10px;
   padding: 10px;
-  box-shadow: 2px 2px 5px #dadada;
-  margin: 20px auto;
+  /* box-shadow: 2px 2px 5px #dadada; */
+  margin: 20px;
+  background: #fff;
+}
+
+.picture {
+  display: flex;
+}
+
+.picture img{
+  width: 9em;
+  height: auto;
+  margin: 0 auto;
 }
 
 .header {
   font-weight: 500;
   text-align: center;
+  color: #2b70dc;
+  font-weight: 500;
+}
+
+ul {
+  padding: 0;
 }
 
 li {
   list-style: none;
-  text-align: start;
+  text-align: center;
+  font-weight: 500;
+  color: #434655;
 }
 
 </style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="root">
-    <div class="heading">Skills</div>
-    <hr />
+    <div class="header">
+      <div class="heading">Skills</div>
+      <hr />
+    </div>
     <div class="container page">
       <skill-card  
       v-for="skill in skills"
-      v-bind:key="skill.id"
-      v-bind:skillHeading="skill.title"
-      v-bind:skillBody="skill.body"
+      :key="skill.id"
+      :skillHeading="skill.title"
+      :skillBody="skill.body"
+      :skillPicture="skill.picture"
       ></skill-card>
     </div>
   </div>
@@ -23,10 +26,10 @@ export default {
   data() {
     return {
       skills: [
-      { id: 1, title: 'Web Development', body: ["VueJS", "NodeJS", "MongoDB", "ExpressJS"]},
-      { id: 2, title: 'Designing', body: ["Abobe Illustrator", "Adobe Photoshop", "Adobe Premiere Pro"]},
-      { id: 3, title: 'Music Production', body: ["FL Studio"]},
-      {id: 4, title: 'Programming', body: ['C / C++', 'Python']}
+      { id: 1, title: 'Web Development', body: ["VueJS", "NodeJS", "MongoDB", "ExpressJS"], picture: require('@/assets/web_dev.png')},
+      { id: 2, title: 'Designing', body: ["Abobe Illustrator", "Adobe Photoshop", "Adobe Premiere Pro", "Adobe Lightroom"], picture: require('@/assets/design.png')},
+      { id: 3, title: 'Music Production', body: ["FL Studio"], picture: require('@/assets/music_prod.png')},
+      {id: 4, title: 'Programming', body: ['C / C++', 'Python'], picture: require('@/assets/programming.png')}
     ]
     };
   }
@@ -39,21 +42,47 @@ export default {
   display: flex;  
 }
 
+.header {
+  background: rgba(0,123,255,.5);
+  margin-top: -140px;
+}
+
 .heading {
   font-family: "Montserrat", sans-serif;
   font-size: 3em;
   font-weight: 500;
-  color: #434655;
+  color: #fff;
   margin-left: 20%;
+  padding-top: 12%;
+  padding-bottom: 50px;
 }
 
 hr {
   width: 80%;
 }
 
+.container, .page {
+  margin-top: -70px;
+}
+
 @media only screen and (max-width: 768px) {
   .container {
     flex-direction: column;
+  }
+  .header {
+    margin-top: -250px;
+  }
+  .heading {
+    padding-top: 30%;
+  }
+}
+
+@media only screen and (max-width: 576px) {
+  .header {
+    margin-top: -300px
+  }
+  .heading {
+    padding-top: 70%;
   }
 }
 
