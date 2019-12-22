0a7b84ea8aac8dc67ce8471c6dfb6b72c44a28ff
naveenbudhwal <naveenbudhwal@gmail.com>
2019-12-22 12:51:08 +0530

    Hamburger menu integration

--- src/App.vue
+++ src/App.vue
@@ -53,10 +53,17 @@
         </li>
       </ul>
 
-      <div class="hamburger">
+      <!-- <div class="hamburger">
         <div class="line one"></div>
         <div class="line"></div>
+      </div> -->
+      
+      <div id="nav-icon1">
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+
     </nav>
 
     <transition
@@ -72,20 +79,37 @@
 
 <script>
 window.addEventListener("load", function() {
-  var hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("open1");
-    const navLinks = document.querySelector(".nav-links");
-    const links = document.querySelectorAll(".nav-links li");
-    navLinks.classList.toggle("open");
-    // line.classList.toggle("open1");
+  const navLinks = document.querySelector(".nav-links");
+  const navIcon = document.querySelector('#nav-icon1');
+  const links = document.querySelectorAll(".nav-links li");
+  navIcon.addEventListener('click', function() {
+    navIcon.classList.toggle('open2');
+    navIcon.classList.toggle('fix');
+    navLinks.classList.toggle("open"); 
     links.forEach(link => {
       link.addEventListener("click", () => {
         navLinks.classList.remove("open");
-        hamburger.classList.remove("open1");
+        // hamburger.classList.remove("open1");
+        navIcon.classList.remove('open2');
+        navIcon.classList.remove('fix');
       });
-    });
-  });
+    });  
+  })
+
+  // var hamburger = document.querySelector(".hamburger");
+  // hamburger.addEventListener("click", () => {
+  //   hamburger.classList.toggle("open1");
+  //   const navLinks = document.querySelector(".nav-links");
+  //   const links = document.querySelectorAll(".nav-links li");
+  //   navLinks.classList.toggle("open");
+  //   line.classList.toggle("open1");
+  //   links.forEach(link => {
+  //     link.addEventListener("click", () => {
+  //       navLinks.classList.remove("open");
+  //       hamburger.classList.remove("open1");
+  //     });
+  //   });
+  // });
 });
 
 export default {
@@ -95,6 +119,87 @@ export default {
 
 
 <style>
+
+#nav-icon1 {
+  display: none;
+  width: 35px;
+  height: 35px;
+  /* position: absolute; */
+  /* margin: 50px auto; */
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .5s ease-in-out;
+  -moz-transition: .5s ease-in-out;
+  -o-transition: .5s ease-in-out;
+  transition: .5s ease-in-out;
+  cursor: pointer;
+  z-index: 2;
+}
+
+#nav-icon1 span {
+  display: block;
+  position: absolute;
+  height: 5px;
+  width: 100%;
+  background: #434655;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .25s ease-in-out;
+  -moz-transition: .25s ease-in-out;
+  -o-transition: .25s ease-in-out;
+  transition: .25s ease-in-out;
+}
+
+#nav-icon1 span:nth-child(1) {
+  opacity: 0;
+  top: 5px;
+}
+
+#nav-icon1 span:nth-child(2) {
+  width: 28px;
+  top: 15px;
+}
+
+#nav-icon1 span:nth-child(3) {
+  top: 25px;
+}
+
+#nav-icon1.open2 span:nth-child(1) {
+  opacity: 1;
+  top: 18px;
+  -webkit-transform: rotate(135deg);
+  -moz-transform: rotate(135deg);
+  -o-transform: rotate(135deg);
+  transform: rotate(135deg);
+}
+
+#nav-icon1.open2 span:nth-child(2) {
+  opacity: 0;
+  left: -60px;
+}
+
+#nav-icon1.open2 span:nth-child(3) {
+  top: 18px;
+  -webkit-transform: rotate(-135deg);
+  -moz-transform: rotate(-135deg);
+  -o-transform: rotate(-135deg);
+  transform: rotate(-135deg);
+}
+
+#nav-icon1.fix {
+  position: fixed;
+  right: 10%;
+}
+
+/* ---------------------------- */
+
 #app {
   font-family: "Montserrat", sans-serif;
 }
@@ -159,9 +264,9 @@ nav a.router-link-exact-active {
   color: #2b70dc;
 }
 
-.hamburger {
+/* .hamburger {
   display: none;
-}
+} */
 
 @media only screen and (max-width: 1200px) {
   .nav-links {
@@ -176,22 +281,25 @@ nav a.router-link-exact-active {
     margin: auto;
   }
 
+  #nav-icon1 {
+    display: block;
+  }
+
   .logo {
     margin-left: -8px;
   }
 
-  .hamburger {
+  /* .hamburger {
     display: block;
-    /* margin-right: 30px; */
     position: absolute;
     right: 0;
     top: 7.5vh;
     transform: translateY(-50%);
     cursor: pointer;
     z-index: 2;
-  }
+  } */
 
-  .line {
+  /* .line {
     width: 25px;
     height: 2.5px;
     background: #000;
@@ -201,16 +309,16 @@ nav a.router-link-exact-active {
 
   .line.one {
     width: 20px;
-  }
+  } */
 /* 
   .line.two {
     transform: rotate(-45deg);
   } */
 
-  .hamburger.open1 {
+  /* .hamburger.open1 {
     position: fixed;
     right: 10%;
-  }
+  } */
 
   .nav-links {
     position: fixed;
@@ -234,7 +342,7 @@ nav a.router-link-exact-active {
     flex-direction: column;
     clip-path: circle(100px at 90% -18%);
     -webkit-clip-path: circle(100px at 90% -18%);
-    transition: all 1s ease-in-out;
+    transition: all 0.8s ease-in-out;
     pointer-events: none;
     z-index: 2;
   }
@@ -248,18 +356,16 @@ nav a.router-link-exact-active {
     transition: all 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
   }
 
-  nav a.router-link-exact-active {
-    background: #fff;
-    color: #2b70dc;
-    padding: 10px 15px;
-    border-radius: 10px;
+  nav .nav-links a.router-link-exact-active {
+    color: #434655;
+    border-bottom: 3px solid black;
+    border-color: #434655;
+    /* padding-left: 10px; */
   }
 
   .nav-links a:hover {
-    background: #fff;
-    color: #2b70dc;
-    padding: 10px;
-    border-radius: 10px;
+    color: #434655;
+    font-size: 1.2em;
   }
 
   .nav-links.open {
